74750728ee35947b5ff337e23955fd876e0f8b21
ethanmartin223 <80828278+ethanmartin223@users.noreply.github.com>
2023-04-15 15:17:35 -0400

    Initial commit

---
+++ obj/src/main.cpp.d
@@ -0,0 +1,15 @@
+obj/src/main.cpp.bc: src\main.cpp \
+  C:\Users\ethan\Documents\CEdev\include\stdint.h \
+  C:\Users\ethan\Documents\CEdev\include\cdefs.h \
+  C:\Users\ethan\Documents\CEdev\include\ti\screen.h \
+  C:\Users\ethan\Documents\CEdev\include\stdbool.h \
+  C:\Users\ethan\Documents\CEdev\include\stddef.h \
+  C:\Users\ethan\Documents\CEdev\include\ti\ui.h \
+  C:\Users\ethan\Documents\CEdev\include\ti\getcsc.h \
+  C:\Users\ethan\Documents\CEdev\include\c++\ti\real \
+  C:\Users\ethan\Documents\CEdev\include\c++\cstdint \
+  C:\Users\ethan\Documents\CEdev\include\c++\cstdlib \
+  C:\Users\ethan\Documents\CEdev\include\stdlib.h \
+  C:\Users\ethan\Documents\CEdev\include\c++\limits \
+  C:\Users\ethan\Documents\CEdev\include\ti\real.h \
+  C:\Users\ethan\Documents\CEdev\include\sys\rtc.h
